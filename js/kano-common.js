335217a1c55a48bdd4f8e0aea3c1ef499f32b740
kano-gooop <plKlength10-43cM>
2022-08-01 10:19:43 +0800

    2022/08/01

--- js/kano-common.js
+++ js/kano-common.js
@@ -1,15 +1,28 @@
 /**
- * 常用函数库（公共）
+ * 常用 函数/常量 库（公共）
  */
 
+const reg = {
+  tel: /^1\d{10}$/,  // 手机号
+  phone: /\d{3,4}-\d{7,8}/,  // 座机（带国家编号）
+  email: /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/,  // 邮箱
+  natural: /^([1-9]\d*|0)$/,  // 自然数
+  positive: /^[1-9]\d*$/,  // 正数
+  price: /^([1-9]\d*|0)(\.\d{1,2})?$/,  // 金额（两位小数点）
+  zh: /[\u4e00-\u9fa5]+/  // 中文
+};
+
 // 获取url参数对象
-function get_params() {
+function get_params()
+{
   let url = location.search;
   let params = {};
-  if (url.indexOf("?") !== -1) {
+  if (url.indexOf("?") !== -1)
+  {
     var str = url.substr(1);
     var strs = str.split("&");
-    for (var i = 0; i < strs.length; i++) {
+    for (var i = 0; i < strs.length; i++)
+    {
       let kv = strs[i].split('='); // key 和 value
       params[kv[0]] = unescape(kv[1]);
     }
@@ -23,10 +36,13 @@ function get_params() {
  * @param fmt  格式化字符串
  * @returns {string}  返回格式化后的字符串
  */
-function time_format(time, fmt = 'yyyy-MM-dd') {
-  if (time) {
+function time_format(time, fmt = 'yyyy-MM-dd')
+{
+  if (time)
+  {
     // 如果是数字类型
-    if (typeof time === 'number') {
+    if (typeof time === 'number')
+    {
       time = new Date(time * 1000);
     }
 
@@ -46,7 +62,8 @@ function time_format(time, fmt = 'yyyy-MM-dd') {
         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
   }
-  else {
+  else
+  {
     return '';
   }
 }
@@ -57,7 +74,8 @@ function time_format(time, fmt = 'yyyy-MM-dd') {
  * @param format  格式化字符串
  * @returns {string}  被格式化的字符串
  */
-function second_format(second, format = 'dd天hh小时mm分ss秒') {
+function second_format(second, format = 'dd天hh小时mm分ss秒')
+{
   let d, h, m, s;  // 日 小时 分钟 秒
 
   d = Math.floor(second / 86400) + '';
@@ -69,7 +87,8 @@ function second_format(second, format = 'dd天hh小时mm分ss秒') {
 
   console.log(d, h, m, s);
 
-  return format.replace(/(d+)|(h+)|(m+)|(s+)/g, (match, day, hour, minute, second) => {
+  return format.replace(/(d+)|(h+)|(m+)|(s+)/g, (match, day, hour, minute, second) =>
+  {
     if (day) return ('0' + d).slice(-Math.max(day.length, d.length));
     if (hour) return ('0' + h).slice(-2);
     if (minute) return ('0' + m).slice(-2);
@@ -78,15 +97,38 @@ function second_format(second, format = 'dd天hh小时mm分ss秒') {
 }
 
 // 重复一个字符串n次
-function repeat(str, count) {
+function repeat(str, count)
+{
   return Array(count + 1).join(str);
 }
 
 // 将字符串转成 unicode 序列
-function unicode(str) {
+function unicode(str)
+{
   var arr = []
-  for (var i = 0; i < str.length; i++) {
+  for (var i = 0; i < str.length; i++)
+  {
     arr.push(str.charCodeAt(i).toString(16));
   }
   return arr;
+}
+
+// 统计字符串中各个字符出现的频率
+function frequency(str)
+{
+  let obj = [];
+
+  for (let char of str)
+  {
+    if (!obj[char])
+    {
+      obj[char] = 1;
+    }
+    else
+    {
+      obj[char]++;
+    }
+  }
+
+  return obj;
 }
